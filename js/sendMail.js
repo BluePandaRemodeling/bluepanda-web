6c31f7a1939c973dd0727f644ee14242ac339823
elghanbibadr <bghanbi50@gmail.com>
2024-10-03 23:34:28 +0100

    replaced img with bathrom image

--- js/sendMail.js
+++ js/sendMail.js
@@ -1,15 +1,25 @@
-console.log("hello badr")
-
-
-function sendMail(){
-    // alert('hello')
-    let params={
-        from_name:document.getElementById("userName").value,
-        email_id:document.getElementById("email_id").value,
-        phone:document.getElementById("phone").value,
-        message:document.getElementById("message").value
-    }
-    emailjs.send("service_klj723p","template_e0yo2zn",params).then((res) =>{
-   alert('You message has been sent successfully' )
-    })
-}
+function sendMail() {
+    let params = {
+      from_name: document.getElementById("userName").value,
+      email_id: document.getElementById("email_id").value,
+      phone: document.getElementById("phone").value,
+      message: document.getElementById("message").value,
+    };
+  
+    // First email sending
+    emailjs.send("service_a019pts", "template_vfydvhc", params)
+      .then((res) => {
+        // If the first email is successful, send the confirmation email
+        return emailjs.send("service_a019pts", "template_tkcu938", params);
+      })
+      .then(() => {
+        // Both emails were successful
+        alert("Your message has been sent successfully");
+      })
+      .catch((error) => {
+        // Handle errors if either of the emails fail
+        console.error("Error sending email:", error);
+        alert("There was an issue sending your message. Please try again later.");
+      });
+  }
+  
